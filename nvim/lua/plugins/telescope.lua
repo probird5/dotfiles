82c745728503845506dc0cec4f782407c6d58bda
probird5 <probird5>
2024-01-06 16:04:39 -0500

    Added linter and formatter for C/C++

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,4 +8,21 @@ return  {
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
   end
+},
+{
+  'nvim-telescope/telescope-ui-select.nvim',
+  config = function()
+  require("telescope").setup({
+    extensions = {
+      ["ui-select"] = {
+        require("telescope.themes").get_dropdown {
+          -- even more opts
+        }
+      }
+    }
+  })
+    require("telescope").load_extension("ui-select")
+    end
 }
+
+
