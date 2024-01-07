119616e5ba207f0743edcf660f05dba0b06a2fcf
probird5 <probird5>
2024-01-07 14:53:21 -0500

    Added autocompletions keybindings

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
@@ -34,6 +34,9 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-c>"] = cmp.mapping.close(),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,6 +4,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap('i', '<C-b>', '<cmd>lua require("cmp").mapping.scroll_docs(-4)<CR>', { noremap = true, silent = true, expr = true })
 
 
