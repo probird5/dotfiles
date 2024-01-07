3b90a4771f7275135c5555f2b0a41d2cf1a522c9
probird5 <probird5>
2024-01-07 11:26:02 -0500

    Added autocompletions + alpha

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,6 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.api.nvim_set_keymap('i', '<C-b>', '<cmd>lua require("cmp").mapping.scroll_docs(-4)<CR>', { noremap = true, silent = true, expr = true })
 
 
