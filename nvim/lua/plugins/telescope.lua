7fbd5ea3b227606dabdeb09b29f944aa7551d4fa
Your Name <you@example.com>
2024-01-08 11:10:59 -0500

    edited telescope keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,8 +5,9 @@ return  {
   config = function()
     local builtin = require("telescope.builtin")
     -- Telescope keybindings
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   end
 },
 {
