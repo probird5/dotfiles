ce4ab5a59e2c656d824e022517d2d3ae43313068
probird5 <probird5>
2024-01-22 15:56:10 -0500

    Added go config as well as autopairs plugin

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,6 +7,8 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.diagnostics.cpplint,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
