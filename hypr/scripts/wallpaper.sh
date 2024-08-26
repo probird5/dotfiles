fefccf5c9814544f7ae2a275619e607b760e44ef
probird5 <probird5>
2024-08-26 11:26:14 -0400

    Updated my hyprland config

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -6,5 +6,7 @@ if ! pgrep -x "swww-daemon" > /dev/null; then
     swww-daemon
 fi
 
+RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
 # Set a random wallpaper from the directory using swww
-swww img  "$WALLPAPER_DIR"
+swww img "$RANDOM_WALLPAPER" --transition-type wipe --transition-angle 30 --transition-step 255 --transition-fps 120 --transition-duration 3
